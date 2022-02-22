be79734c2fb31722e76b91a0e428e07022554025
Ankit <radheyshyam91647127@gmail.com>
2022-02-22 21:34:34 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // contact list without using mongo db database
 const express=require('express');
 const app= express();
-const port=3000;
+const port= process.env.PORT || 3000;
 const path=require('path');
 const fs= require('fs');
 
